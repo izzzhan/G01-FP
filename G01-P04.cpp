4c1f36c39f022a5dad5550d0a450869460ae7aea
izzzhan <83181550+izzzhan@users.noreply.github.com>
2021-10-19 13:16:57 +0200

    Create G01-P04.cpp

---
+++ G01-P04.cpp
@@ -0,0 +1,28 @@
+ //
+ //  Guión 1 - Tema 2
+ //  4. Diseñe un programa que intercambie el valor de dos variables de tipo entero leídas desde teclado, sacando su
+ //  valor por pantalla.
+ //
+ //  @izzzhan
+ //
+
+ #include <iostream>
+
+ using namespace std;
+
+ int main() {
+     int a, b, aux;
+
+     cout << "Digite un valor para a: "; cin >> a;
+     cout << "Digite un valor para b: "; cin >> b;
+     cout << "\nba = " << a << "\nb = " << b;
+
+     // CAMBIAMOS LAS VARIABLES
+     aux = a;
+     a = b;
+     b = aux;
+
+     cout << "\nEl nuevo valor de a y b es: " << "\n a = " << a << "\n b = " << b << endl;
+
+     return 0;
+ }
