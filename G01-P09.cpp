bdf8c1fd3938ac860dfc8a8068115e761943b5d2
izzzhan <83181550+izzzhan@users.noreply.github.com>
2021-10-21 11:38:07 +0200

    Create G01-P09.cpp

---
+++ G01-P09.cpp
@@ -0,0 +1,30 @@
+//
+//  Tema 2
+//  9. Determine cuáles de los siguientes identificadores son válidos. Si son inválidos explique por qué (pruébelos en el compilador):
+//  a) registro 1     b)1registro1          c)archivo_3           d) float       
+//  e) $impuesto      f) _nombre            g) nombre A           h) 123_opcion 
+//  i) Belen          j) Resultado_total    k) N_pasaje3          l) Float
+//
+//  @izzzhan
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int registro 1;         // Inválido. No se puden poner espacios.
+    int 1registro1;         // Inválido. No se puede empezar con un número.
+    int archivo_3;          // Válido
+    int float;              // Inválido. No se pueden usar palabras registradas.
+    int $impuesto;          // Válido
+    int _nombre;            // Válido
+    int nombre A;           // Invalido. No puede haber espacios.
+    int 123_opcion;         // Inválido. No se puede empezar con un número.
+    int Belen;              // Válido
+    int Resultado_Total;    // Válido
+    int N_pasaje3;          // Válido
+    int Float;              // Válido
+  
+  return 0;
+}
