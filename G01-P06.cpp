086c8bec3bd6b7afe22091427e110bf6af09714d
izzzhan <83181550+izzzhan@users.noreply.github.com>
2021-10-21 11:35:58 +0200

    Update G01-P06.cpp

--- G01-P06.cpp
+++ G01-P06.cpp
@@ -2,7 +2,7 @@
 //  Guión 1 - Tema 2
 //  6. Diseñe un programa en C++ de tal forma que pedirá la nota de teoría y la nota de prácticas de 
 //  un examen realizado (se suponen valoradas entre 0 y 10). Y mostrará por pantalla la nota final, sabiendo que la nota 
-//  de teoría vale el 70% y la nota de prácticas vale el 30%.
+//  de teoría vale el 40% y la nota de prácticas vale el 60%.
 //
 //  @izzzhan
 //
@@ -14,7 +14,7 @@ using namespace std;
 int main(){
   float teoria;         // Nota del examen de teoría.
   float practicas;      // Nota del examen de prácticas.
-  float media;          // Nota media; 0.7 de teoría y 0.3 de prácticas.
+  float media;          // Nota media; 0.4 de teoría y 0.6 de prácticas.
   
   cout << "Digite su nota de teoria: ";
   cin >> teoria;
@@ -22,7 +22,7 @@ int main(){
   cin >> practicas;
   
   // Calculamos su nota media
-  media = 0.7 * teoria + 0.3 * practicas;
+  media = 0.4 * teoria + 0.6 * practicas;
   
   cout << "\nSu nota media es de: " << media << " puntos." << endl;
   
